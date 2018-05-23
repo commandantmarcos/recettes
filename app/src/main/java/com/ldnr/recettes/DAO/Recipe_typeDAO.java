2456575f10ef7be94b575920037b1a4e0503a9ac
Pauline <preel.pauline@gmail.com>
2018-05-23 20:44:23 +0200

    Codage des méthodes utiles

--- app/src/main/java/com/ldnr/recettes/DAO/Recipe_typeDAO.java
+++ app/src/main/java/com/ldnr/recettes/DAO/Recipe_typeDAO.java
@@ -4,76 +4,118 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 
-import com.ldnr.recettes.Beans.Have;
 import com.ldnr.recettes.Beans.Recipe_type;
-import com.ldnr.recettes.ConnectionBDD.DBHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe_typeDAO extends DAO implements IRecipe_typeDAO {
 
     private Cursor cursor;
     private ContentValues initialValues;
-    private String[] allColumns = { dbHelper.INGREDIENT_ID_INGREDIENT, dbHelper.RECIPE_ID_RECIPE, dbHelper.UNITY_ID_UNITY, dbHelper.HAVE_INGR_COUNT };
+    private String[] allColumns = { dbHelper.RECIPE_TYPE_ID_TYPE, dbHelper.RECIPE_TYPE_TYPE_NAME };
     private Recipe_type recipe_type = null;
 
+    /**
+     * Constructor by default
+     * @param context
+     */
     public Recipe_typeDAO(Context context) {
         super(context);
     }
 
     @Override
     public Recipe_type create(Recipe_type new_recipe_type) {
+        // TODO : Création d'un type de plat
+        // Uniquement quand le client aura validé et payé !
         return null;
     }
 
     @Override
     public Recipe_type find(int id) {
-        return null;
+        open();
+        cursor = database.rawQuery( "select " + allColumns + " from " + dbHelper.TABLE_USER_NAME + " WHERE " + dbHelper.RECIPE_TYPE_ID_TYPE + " = " + id + " ; ", null );
+        cursor.moveToFirst();
+
+        if( cursor != null && cursor.moveToFirst() ) {
+            recipe_type = new Recipe_type( (cursor.getInt(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_ID_TYPE))), (cursor.getString(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_TYPE_NAME))) ) ;
+        }
+        cursor.close();
+
+        return recipe_type;
     }
 
     @Override
     public Recipe_type find(String name) {
-        return null;
+        open();
+        cursor = database.rawQuery( "select " + allColumns + " from " + dbHelper.TABLE_USER_NAME + " WHERE " + dbHelper.RECIPE_TYPE_TYPE_NAME + " = " + name + " ; ", null );
+        cursor.moveToFirst();
+
+        if( cursor != null && cursor.moveToFirst() ) {
+            recipe_type = new Recipe_type( (cursor.getInt(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_ID_TYPE))), (cursor.getString(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_TYPE_NAME))) ) ;
+        }
+        cursor.close();
+
+        return recipe_type;
     }
 
     @Override
     public List<Recipe_type> findAll(String name) {
+        // TODO : Recherche de tous les types par nom
+        // Payable en cash !
         return null;
     }
 
     @Override
     public List<Recipe_type> findAll(int id) {
+        // TODO : Recherche de tous les types par id
+        // Possiblité de troquer la fonctionnalité contre un séjour d'un weekend à Londres TCC !
         return null;
     }
 
     @Override
     public List<Recipe_type> findAll() {
-        return null;
+        List<Recipe_type> listType = new ArrayList<>();
+        cursor = database.rawQuery("SELECT "+ allColumns + " FROM " + dbHelper.TABLE_RECIPE_TYPE_NAME + " ; " , null );
+        cursor.moveToFirst();
+
+        if( cursor != null && cursor.moveToFirst() ){
+            while(!cursor.isAfterLast()) {
+                listType.add(new Recipe_type((cursor.getInt(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_ID_TYPE))), (cursor.getString(cursor.getColumnIndex(dbHelper.RECIPE_TYPE_TYPE_NAME)))) );
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+
+        return listType;
     }
 
     @Override
     public void update(Recipe_type new_recipe_type) {
-
+        // TODO : Mise à jour d'un type de plat
+        // Pour le prix d'une rate seulement !
     }
 
     @Override
     public void delete(Recipe_type old_recipe_type) {
-
+        // TODO : Suppression d'un type de plat
+        // Uniquement quand une autre main est mise à la poche !
     }
 
+    /**
+     * Méthode permettant de renvoyer les valeurs du type initialisé
+     * @return initialValues : ContentValues
+     */
     public ContentValues getInitialValues() {
         return initialValues;
     }
 
+    /**
+     * Méthode permettant de rentrer les valeurs du type à initialiser
+     * @param recipe_type : Recipe_type à créer
+     */
     public void setInitialValues(Recipe_type recipe_type) {
-        this.initialValues = initialValues;
-
-        initialValues.put(dbHelper.INGREDIENT_ID_INGREDIENT, new_have.getIngredient().getId_ingredient());
-
-        // TODO : Réussir à récupérer l'id-recipe !!
-        // contentValues.put(dbHelper.RECIPE_ID_RECIPE, new_have.getIngredient());
-
-        initialValues.put(dbHelper.UNITY_ID_UNITY, new_have.getUnity() );
-        initialValues.put(dbHelper.HAVE_INGR_COUNT, new_have.getNumber() );
+        this.initialValues = new ContentValues();
+        initialValues.put(dbHelper.RECIPE_TYPE_TYPE_NAME, recipe_type.getType_name());
     }
 }
