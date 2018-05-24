5bb33dfa117c370e041d871775f80116b6f3756a
Benjamin <benjamin.desfontaines@gmail.com>
2018-05-24 19:41:51 +0200

    Récupération de l'id_recipe dans la dao step

--- app/src/main/java/com/ldnr/recettes/DAO/StepDAO.java
+++ app/src/main/java/com/ldnr/recettes/DAO/StepDAO.java
@@ -2,6 +2,7 @@ package com.ldnr.recettes.DAO;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 
 import com.ldnr.recettes.Beans.Step;
 
@@ -38,9 +39,10 @@ public class StepDAO extends DAO implements IStepDAO {
         int num = res.getInt(res.getColumnIndex(dbHelper.STEP_STEP_NUM));
         String url = res.getString(res.getColumnIndex(dbHelper.STEP_URL_STEP));
         String description = res.getString(res.getColumnIndex(dbHelper.STEP_STEP_DESCRIBE));
+        int id_recipe = res.getInt(res.getColumnIndex(dbHelper.RECIPE_ID_RECIPE));
 
 
-        step = new Step(id_step, num, url, description);
+        step = new Step(id_step, num, url, description, id_recipe);
 
         res.close();
 
@@ -70,9 +72,10 @@ public class StepDAO extends DAO implements IStepDAO {
             int num = res.getInt(res.getColumnIndex(dbHelper.STEP_STEP_NUM));
             String url = res.getString(res.getColumnIndex(dbHelper.STEP_URL_STEP));
             String description = res.getString(res.getColumnIndex(dbHelper.STEP_STEP_DESCRIBE));
+            int id_recipe = res.getInt(res.getColumnIndex(dbHelper.RECIPE_ID_RECIPE));
 
 
-            steps.add(new Step(id_step, num, url, description));
+            steps.add(new Step(id_step, num, url, description, id_recipe));
 
             res.moveToNext();
         }
@@ -93,9 +96,10 @@ public class StepDAO extends DAO implements IStepDAO {
             int num = res.getInt(res.getColumnIndex(dbHelper.STEP_STEP_NUM));
             String url = res.getString(res.getColumnIndex(dbHelper.STEP_URL_STEP));
             String description = res.getString(res.getColumnIndex(dbHelper.STEP_STEP_DESCRIBE));
+            int id_recipe = res.getInt(res.getColumnIndex(dbHelper.RECIPE_ID_RECIPE));
 
 
-            steps.add(new Step(id_step, num, url, description));
+            steps.add(new Step(id_step, num, url, description, id_recipe));
 
             res.moveToNext();
         }
@@ -116,9 +120,10 @@ public class StepDAO extends DAO implements IStepDAO {
 
     public void delete(List<Step> old_step) {
         open();
+        Log.d("DAO DELETE STEPS", Integer.toString(old_step.size()));
         for(int i = 0; i < old_step.size(); i++) {
-            database.delete(dbHelper.TABLE_STEP_NAME, dbHelper.STEP_ID_STEP + " = ?",
-                    new String[]{String.valueOf(old_step.get(i).getId_step())});
+            database.delete(dbHelper.TABLE_STEP_NAME, dbHelper.RECIPE_ID_RECIPE + " = ?",
+                    new String[]{String.valueOf(old_step.get(i).getId_recipe())});
         }
         close();
 
