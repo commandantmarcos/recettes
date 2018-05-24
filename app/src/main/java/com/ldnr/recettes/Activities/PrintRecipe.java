fdd0823ee277bdc1c5aa8cc90953a68541e98c97
Pauline <preel.pauline@gmail.com>
2018-05-24 21:39:23 +0200

    Corrections finales

--- app/src/main/java/com/ldnr/recettes/Activities/PrintRecipe.java
+++ app/src/main/java/com/ldnr/recettes/Activities/PrintRecipe.java
@@ -8,6 +8,9 @@ package com.ldnr.recettes.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -16,20 +19,25 @@ import android.widget.TextView;
 
 import com.ldnr.recettes.Beans.Have;
 import com.ldnr.recettes.Beans.Recipe;
+import com.ldnr.recettes.Beans.Step;
+import com.ldnr.recettes.DAO.HaveDAO;
 import com.ldnr.recettes.DAO.RecipeDAO;
+import com.ldnr.recettes.DAO.StepDAO;
 import com.ldnr.recettes.R;
+import com.ldnr.recettes.Views.AdapterPrint;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrintRecipe extends AppCompatActivity {
 
-    private TextView recipeTitle;
-    private ImageView recipePicture;
-    private TextView recipeDetails;
-    private TextView ingTitle;
-    private ListView listIng;
-    private ListView listStep;
+    private RecipeDAO daoRecipe;
+    private HaveDAO daoHave;
+    private StepDAO daoStep;
 
-    Recipe recipe;
+    private List<Have> listHave = new ArrayList<>();
+    private List<Step> tabStep = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,62 +45,64 @@ public class PrintRecipe extends AppCompatActivity {
         setContentView(R.layout.print_recipe);
 
         // Get widgets
-        recipeTitle = findViewById(R.id.p_recipe_title);
-        recipePicture = findViewById(R.id.p_recipe_picture);
-        recipeDetails = findViewById(R.id.p_recipe_description);
-        ingTitle = findViewById(R.id.p_recipe_ing);
-        listIng = findViewById(R.id.p_recipe_ing_list);
-        listStep = findViewById(R.id.p_recipe_step_list);
+        TextView recipeTitle = findViewById(R.id.p_recipe_title);
+        ImageView recipePicture = findViewById(R.id.p_recipe_picture);
+        TextView recipeDetails = findViewById(R.id.p_recipe_description);
+        TextView ingTitle = findViewById(R.id.p_recipe_ing);
+        ListView listIng = findViewById(R.id.p_recipe_ing_list);
+        RecyclerView listStep = findViewById(R.id.p_recipe_step_list);
 
         // Get Extra
         Intent intent = getIntent();
-        String id = intent.getStringExtra("id");
+        int id = intent.getIntExtra("id", 0);
 
         // Get Recipe clicked
-        RecipeDAO daoRecipe = new RecipeDAO(this.getBaseContext());
-        recipe = daoRecipe.find(id);
+        daoRecipe = new RecipeDAO(this);
+        Recipe recipe = daoRecipe.find(id);
 
-        // Set text and image for the recipe
-        recipeTitle.setText(recipe.getName());
-        Picasso.with(recipePicture.getContext()).load(recipe.getUrl_picture()).centerCrop().fit().into(recipePicture);
+        Log.e("Null Exception ", "###################################### "+ id+" ###########################################");
 
-        // Construction of string for the recipe's details
-        StringBuilder buffer = new StringBuilder();
-        buffer.append("Recette ");
-        buffer.append(recipe.getDish_type().getType_name());
-        buffer.append("\n");
-        buffer.append("Pour ");
-        buffer.append(recipe.getServings_count());
-        buffer.append(" personnes\n");
-        buffer.append("Postée par : ");
-        buffer.append(recipe.getUser().getLogin());
-        recipeDetails.setText(buffer);
-
-        //
-        ArrayAdapter<String> tabIng = new ArrayAdapter<>(listIng.getContext(), R.layout.my_ingredient, R.id.my_ing_text);
-        for (Have haveIng : recipe.getListIngredient()) {
-            StringBuilder ing = new StringBuilder();
-            ing.append(haveIng.getNumber());
-            ing.append(" ");
-            ing.append(haveIng.getUnity());
-            ing.append(" de ");
-            ing.append(haveIng.getIngredient().getName_ingr());
-            tabIng.add(ing.toString());
-        }
-        listIng.setAdapter(tabIng);
-/**
-        ArrayAdapter<Step> tabStep = new ArrayAdapter<>(listIng.getContext(), R.layout.my_ingredient);
-        for (Step step : recipe.getSteps()) {
-
-
+            // Set text and image for the recipe
             recipeTitle.setText(recipe.getName());
             Picasso.with(recipePicture.getContext()).load(recipe.getUrl_picture()).centerCrop().fit().into(recipePicture);
 
+            // Construction of string for the recipe's details
+            StringBuilder buffer = new StringBuilder();
+            buffer.append("Recette ");
+            buffer.append(recipe.getDish_type().getType_name());
+            buffer.append("\n");
+            buffer.append("Pour ");
+            buffer.append(recipe.getServings_count());
+            buffer.append(" personnes\n");
+            buffer.append("Postée par : ");
+            buffer.append(recipe.getUser().getLogin());
+            recipeDetails.setText(buffer);
 
-            this.initialValues.put(dbHelper.STEP_ID_STEP, step.getId_step() );
-        }
-        listStep.setAdapter(tabStep);**/
+            //
+            ArrayAdapter<String> tabIng = new ArrayAdapter<>(listIng.getContext(), R.layout.my_ingredient, R.id.my_ing_text);
 
+            daoHave = new HaveDAO(this);
+            listHave = daoHave.findAll(id);
+
+            for (Have haveIng : listHave) {
+                StringBuilder ing = new StringBuilder();
+                ing.append(haveIng.getNumber());
+                ing.append(" ");
+                ing.append(haveIng.getUnity());
+                ing.append(" de ");
+                ing.append(haveIng.getIngredient().getName_ingr());
+                tabIng.add(ing.toString());
+            }
+            if (!tabIng.isEmpty()) {
+                listIng.setAdapter(tabIng); }
+
+        listStep = findViewById(R.id.p_recipe_step_list);
+        listStep.setLayoutManager(new LinearLayoutManager(this));
+
+            daoStep = new StepDAO(this);
+            tabStep = daoStep.findAll(id);
+
+        listStep.setAdapter(new AdapterPrint(tabStep));
 
     }
 
